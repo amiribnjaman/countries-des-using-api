0a289f06ce8c25dff963c6f86dce06dff055adc0
amiribnjaman <amiribnjaman@gmail.com>
2022-02-24 12:41:52 +0600

    All countries details using api

---
+++ country.js
@@ -0,0 +1,50 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v2/all')
+    .then(res => res.json())
+    .then(data => displayAllContries(data))
+}
+
+loadCountries()
+
+const displayAllContries = countries => {
+    const allCountries = document.getElementById('display-countries')
+    for(const country of countries){
+        console.log(country)
+        const div = document.createElement('div')
+        div.classList.add('col-md-4')
+        div.innerHTML = `
+            <div class="card my-2">
+                <div class="card-body">
+                <div class="d-flex">
+                    <img src="${country.flag}" style="width:30px"/>
+                    <h6><span class="fw-bold ms-2">${country.name}</span><h6>
+                </div>
+                    <h6><span class="fw-bold">Capital: </span>${country.capital}<h6>
+                    <h6><span class="fw-bold">Area: </span>${country.area}<h6>
+                    <h6><span class="fw-bold">Calling code: </span>${country.callingCodes[0]}<h6>
+                    <h6><span class="fw-bold">Calling code: </span>${country.callingCodes[0]}<h6>
+                    <h6><span class="fw-bold">Calling code: </span>${country.callingCodes[0]}<h6>
+                    <h6><span class="fw-bold">Timezone: </span>${country.timezones[0]}<h6>
+                    <h6><span class="fw-bold">Population: </span>${country.population}<h6>
+                    <h6><span class="fw-bold">Top level domain: </span>${country.topLevelDomain[0]}<h6>
+                    <h6 class="fw-bold"><span class="fw-bold">Region: </span>${country.region}<h6>
+                    
+                </div>
+            </div>
+        `
+        allCountries.appendChild(div)
+        
+
+    }
+}
+
+const currencies = (currency) => {
+    for(const key in currency){
+        return currency[key]
+    }
+}
+
+// Asia region countries
+const loadAsionnCountries = () => {
+    console.log('00')
+}
